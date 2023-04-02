bad35afd21021ee42e1d808565cc6b8c1135d559
linedev06 <vitorafelismino@gmail.com>
2023-04-01 23:20:41 -0300

    exercicio feito

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int  primeironumero, segundonumero, soma;
+
+Console.WriteLine("--- Calculo entre numeros --- ");
+
+
+Console.WriteLine("digite o primeiro numero:");
+primeironumero = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("digite o segundo numero:");
+segundonumero = Convert.ToInt32(Console.ReadLine());
+
+soma = primeironumero + segundonumero;
+
+Console.Write($"\nSoma: {soma}");
